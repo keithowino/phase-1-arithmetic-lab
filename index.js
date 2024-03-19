ade56ac418afe8b438a98fcacd574dffc12eec6c
keithowino <designsolutions1629@gmail.com>
2024-03-19 13:46:49 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+// Multiply
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+// Random
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Modulus
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+// Max
+
+let max = Math.max(1, 10, 20);
